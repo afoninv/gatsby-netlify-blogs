11bb2b703bbbf0204a21e09f05dd5109557244a0
Sergey Efimov <sergey.efimov@insolar.io>
2021-02-10 22:48:45 +0300

    Correct build

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -67,7 +67,7 @@ const TemplateWrapper = ({ children }) => {
           src="https://www.googletagmanager.com/ns.html?id=GTM-W7C2KQB"
           height="0"
           width="0"
-          style="display:none;visibility:hidden"/>
+          style={{display: "none", visibility: "hidden"}}/>
       </noscript>
 
       <Helmet>
